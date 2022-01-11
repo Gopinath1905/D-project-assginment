b3a01b625691d7665a16cf492cebb30edb441bf2
Gopinath1905 <95068638+Gopinath1905@users.noreply.github.com>
2022-01-11 10:58:07 +0530

    commit

---
+++ js/basic.js
@@ -0,0 +1,30 @@
+//first question
+/*let fname="john"
+alert(fname)
+console.log(fname)*/
+//4th
+/*operaters*/
+/*console.log(5>4)*/
+/*console.log("apple">"pineapple")*/
+/*console.log("2">"12")*/
+/*console.log(undefined==null)*/
+/*console.log(undefined===null)*/
+/*console.log(null=="\n0\n")*/
+/*console.log(null===+"\n0\n")*/
+/*ask for a name
+let name=prompt("Enter your name")
+console.log(name)*/
+//variable declaration
+/*let a=10
+console.log(a)
+console.log(typeof a)
+let b="peter"
+console.log(b)
+console.log(typeof b)*/
+/*let student_name=["john"]
+console.log(student_name)
+console.log(typeof (student_name))*/
+//colour
+/*const yellow="#f3ca28"
+console.log(yellow)*/
+
---
+++ js/lvl 2.js
@@ -0,0 +1,66 @@
+/*//1st
+let num;
+do{
+    num=prompt("enter a number greater than 100",0)
+}
+while(num>=100)*/
+
+/*//2nd
+let num=prompt("enter a number")
+if(num>0){
+    alert("value is greater than zero")
+}
+else if(num<0){
+    alert("value is less than zero")
+}
+else if(num==0){
+    alert("value is equal zero")
+}   
+else{
+    console.log(num)
+}*/
+
+/*//4th
+let browser=prompt("Enter the browser name")
+if(browser=="Edge"){
+    alert("You've got the Edge");
+}
+else if(browser=="Chrome"||"Firefox"||"Safari"||"Opera"){
+    alert("OK We support these Browsers");
+}
+else {
+    alert("We hope that this page looks OK!")
+}
+*/
+/*//5th
+let a=prompt("a?',")
+if(a==0){
+    alert(0);
+}
+else if(a==1){
+    alert(1);
+}
+else if(a==2||a==3){
+    alert('2,3');
+}
+else {
+    alert("none")
+}*/
+
+/*//6th
+mun=(a,b)=>{
+  return Math.min(a,b)
+}
+console.log(mun(2,5))
+console.log(mun(3,-1))
+console.log(mun(1,1))*/
+
+/*//7th
+
+ pow=(x,n)=>{
+ return x**n ;
+
+}
+console.log(pow(3,2))
+console.log(pow(3,3))
+console.log(pow(1,100))*/
